48eb96dcf3c5aea239ae9251bce6c5cde9169ec0
Karol Piecuch <139210946+VillageR88@users.noreply.github.com>
2024-06-12 18:07:04 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Kumbh_Sans, Roboto_Slab, Space_Mono } from 'next/font/google';
+import DataProvider from '@/app/_providers/DataContext';
+
+import { ReactNode } from 'react';
+
+const kumbhSans = Kumbh_Sans({
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-kumbh-sans',
+  subsets: ['latin'],
+});
+const robotoSlab = Roboto_Slab({
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-roboto-slab',
+  subsets: ['latin'],
+});
+
+const spaceMono = Space_Mono({
+  display: 'swap',
+  weight: ['400', '700'],
+  variable: '--font-space-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Pomodoro',
+  description: 'Pomodoro app',
+  applicationName: 'Pomodoro app',
+} as const;
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html className="hidden" lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <meta property="og:image" content={undefined} />
+      </head>
+      <body className={`${kumbhSans.variable} ${robotoSlab.variable} ${spaceMono.variable} bg-[#1E213F]`}>
+        <DataProvider>{children}</DataProvider>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        evenSmallerScreen: '240px',
+        verySmallScreen: '360px',
+        tablet: '700px',
+        screen840: '840px',
+        screen1200: '1200px',
+        screenInBetween: '1024px',
+      },
+      fontFamily: {
+        kumbhSans: ['var(--font-kumbh-sans)'],
+        robotoSlab: ['var(--font-roboto-slab)'],
+        spaceMono: ['var(--font-space-mono)'],
+      },
+      colors: {
+        gray1: '#121212',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+};
+
+export default config;
