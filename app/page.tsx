5c2eb1d707faed857884ee9db13cecb1663cafc5
VillageR88 <villager88pl@gmail.com>
2024-06-13 11:00:39 +0200

    layout tryout1
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/app/home/Navbar';
+import Main from '@/app/home/Main';
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       className={`z-0 flex min-h-dvh flex-col items-center justify-start p-[40px] font-libreBaskerville sm:min-h-screen`}
     >
       <Navbar />
+      <Main />
     </div>
   );
 }
