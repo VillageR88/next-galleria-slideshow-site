b5be5c5af69f5dead686eb592ec2e014713a8ff3
VillageR88 <villager88pl@gmail.com>
2024-06-14 11:32:06 +0200

    chore: Update Main component to use lazy loading for images
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Main.tsx
+++ app/home/Main.tsx
@@ -3,14 +3,17 @@ import Image from 'next/image';
 
 export default function Main() {
   return (
-    <div className="mt-[40px] grid size-full grid-cols-4 gap-[40px]">
+    <div className="mt-[40px] grid size-fit grid-cols-4 gap-[40px]">
       {dataJson.map((data, index) => {
         return (
-          <div key={index} className="relative flex size-[400px] flex-col items-center justify-center gap-[20px]">
-            <Image className="absolute object-scale-down" src={data.images.thumbnail} fill alt={data.name} />
-            <h2 className="z-10 text-center text-lg text-white">{data.name}</h2>
-            <p className="z-10 text-white">{data.artist.name}</p>
-          </div>
+          <Image
+            key={index}
+            src={data.images.thumbnail}
+            width={0}
+            height={0}
+            className="size-fit object-scale-down"
+            alt={data.name}
+          />
         );
       })}
     </div>
