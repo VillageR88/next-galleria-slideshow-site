2a24a8cd9fbc68475c50ab77bbc14266893ec279
VillageR88 <villager88pl@gmail.com>
2024-06-14 23:43:16 +0200

    tablet layer1
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Main.tsx
+++ app/home/Main.tsx
@@ -1,12 +1,14 @@
+'use client';
 import dataJson from '@/public/assets/data.json';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-const Column = ({ array }: { array: number[] }) => (
+const Column = ({ array, classExtension }: { array: number[]; classExtension?: string }) => (
   <div className="flex flex-col gap-[40px]">
     {dataJson.map((data, index) => {
       if (array.includes(index))
         return (
-          <div className="relative" key={index}>
+          <div className={`relative ${classExtension ? classExtension : ''}`} key={index}>
             <Image src={data.images.thumbnail} width={10} height={10} className="size-fit" alt={data.name} />
             {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
             <div className="to absolute inset-0 flex min-h-[170px] w-full flex-col justify-end gap-[7px] self-end bg-gradient-to-b from-[#000000]/0 to-[#000000]/[84%] px-[32px] pb-[32px]">
@@ -21,12 +23,33 @@ const Column = ({ array }: { array: number[] }) => (
   </div>
 );
 export default function Main() {
+  const [array1, setArray1] = useState<number[]>([0, 4, 8, 11]);
+  const [array2, setArray2] = useState<number[]>([1, 5, 9, 12]);
+  const array3 = [2, 6, 13];
+  const array4 = [3, 7, 10, 14];
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1280) {
+        setArray1([0, 4, 8, 11, 2, 6, 13]);
+        setArray2([1, 5, 9, 12, 3, 7, 10, 14]);
+      } else {
+        setArray1([0, 4, 8, 11]);
+        setArray2([1, 5, 9, 12]);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="mt-[40px] grid size-fit grid-cols-4 gap-[40px]">
-      <Column array={[0, 4, 8, 11]}></Column>
-      <Column array={[1, 5, 9, 12]}></Column>
-      <Column array={[2, 6, 13]}></Column>
-      <Column array={[3, 7, 10, 14]}></Column>
+    <div className="mt-[40px] grid size-fit grid-cols-2 gap-[40px] xl:grid-cols-4">
+      <Column array={array1}></Column>
+      <Column array={array2}></Column>
+      <Column classExtension="xl:block hidden" array={array3}></Column>
+      <Column classExtension="xl:block hidden" array={array4}></Column>
     </div>
   );
 }
