cb7742032bfc637c48147440e09188e7c6e431b0
VillageR88 <villager88pl@gmail.com>
2024-06-18 13:37:31 +0200

    buttons logic improved
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/ButtonPrevious.tsx
+++ app/components/ButtonPrevious.tsx
@@ -1,7 +1,18 @@
+'use client';
+import { useEffect, useRef } from 'react';
 export default function ButtonPrevious({ clicked, disabled }: { clicked(): void; disabled: boolean }) {
+  useEffect(() => {
+    const move = () => buttonRef.current?.classList.add('transition-colors');
+    window.addEventListener('mousemove', move);
+    return () => {
+      window.removeEventListener('mousemove', move);
+    };
+  }, []);
+  const buttonRef = useRef<HTMLButtonElement>(null);
   return (
     <button
-      className={`stroke-black transition-colors disabled:stroke-black/15 [&:not(:disabled)]:hover:stroke-black/50`}
+      ref={buttonRef}
+      className={`stroke-black disabled:stroke-black/15 [&:not(:disabled)]:hover:stroke-black/50`}
       disabled={disabled}
       onClick={clicked}
       title="Previous slide"
