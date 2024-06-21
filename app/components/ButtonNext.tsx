d5f87c4597b1253fac733fd317f46c44f55b0196
Karol Piecuch <villager88pl@gmail.com>
2024-06-21 09:27:18 +0200

    merge dev6

--- app/components/ButtonNext.tsx
+++ app/components/ButtonNext.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useEffect, useRef } from 'react';
-export default function ButtonNext({ clicked, disabled }: { clicked(): void; disabled: boolean }) {
+import Link from 'next/link';
+import dataJson from '@/public/assets/data.json';
+export default function ButtonNext({ nextIndex, disabled }: { nextIndex: number; disabled: boolean }) {
   useEffect(() => {
     const move = () => buttonRef.current?.classList.add('transition-colors');
     window.addEventListener('mousemove', move);
@@ -14,16 +16,20 @@ export default function ButtonNext({ clicked, disabled }: { clicked(): void; dis
       ref={buttonRef}
       className={`scale-75 stroke-black disabled:stroke-black/15 md:scale-100 [&:not(:disabled)]:hover:stroke-black/50`}
       disabled={disabled}
-      onClick={clicked}
       title="Next slide"
       type="button"
     >
-      <svg width="26" height="24" xmlns="http://www.w3.org/2000/svg">
-        <g fill="none" fillRule="evenodd">
-          <path d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z" strokeWidth="2" />
-          <path fill="#D8D8D8" d="M24.708.5h1v22.775h-1z" />
-        </g>
-      </svg>
+      <Link
+        className={dataJson[nextIndex] ? 'pointer-events-auto' : 'pointer-events-none'}
+        href={dataJson[nextIndex] ? `/detail/${dataJson[nextIndex].name.replace(/ /g, '_')}` : '#'}
+      >
+        <svg width="26" height="24" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fillRule="evenodd">
+            <path d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z" strokeWidth="2" />
+            <path fill="#D8D8D8" d="M24.708.5h1v22.775h-1z" />
+          </g>
+        </svg>
+      </Link>
     </button>
   );
 }
